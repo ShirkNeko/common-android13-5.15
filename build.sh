fa9e0d56347726a48bdd89cb6b039740e0705152
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-07-03 17:25:52 +0800

    Add Sukisu subrepo

---
+++ build.sh
@@ -0,0 +1,20 @@
+export PATH=/root/kernel/tool/google_clang21/bin:$PATH
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
+
+git submodule init
+git submodule update
+
+rm -rf out
+
+make LLVM=1 LLVM_IAS=1 ARCH=arm64 CC="ccache clang" HOSTCC="ccache clang" HOSTCXX="ccache clang++" O=out gki_defconfig
+
+scripts/config --file out/.config \
+      -e LTO_CLANG \
+      -d LTO_NONE \
+      -e LTO_CLANG_THIN \
+      -d LTO_CLANG_FULL \
+      -e THINLTO \
+      -d ARM64_BTI_KERNEL 
+      
+make LLVM=1 LLVM_IAS=1 ARCH=arm64 CC="ccache clang" HOSTCC="ccache clang" HOSTCXX="ccache clang++" O=out -j12 2>&1 | tee build.log
